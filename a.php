1e20479c302d9a12fd24736f2b0a39608bdc0b4f
takeuchimio <miwakotaro0416@yahoo.co.jp>
2020-06-24 13:18:55 +0000

    PHP/Laravel02

---
+++ a.php
@@ -0,0 +1,40 @@
+<?php
+echo 'hello php!' . "\n";
+$animals = [
+    "cat" => "猫",
+    "dog" => "犬",
+    "bird" => "鳥"
+    ];
+    
+echo $animals [ "cat" ] . "\n";
+
+$a = 3;
+$b = 7;
+echo $a + $b . "\n";
+$array_month = ["1月", "２月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month [7]. "\n";
+$hello = 'Hello.';
+$name = 'Mio';
+$world = "'s World!";
+echo $hello . $name . $world .  "\n";
+$tech_boost = "tech";
+echo $tech_boost . "boost" . "\n";
+
+$carendar_2018 = [
+    "January" => "1月",
+    "February" => "2月",
+    "March" => "3月",
+    "April" => "4月",
+    "May" => "5月",
+    "Jun" => "6月",
+    "July" => "7月",
+    "August " => "8月",
+    "September" => "9月",
+    "October" => "10月",
+    "Nobember" => "11月",
+    "December" => "12月",
+    ];
+    echo $carendar_2018 [ "December" ] . "\n";
+    
+?>
+
